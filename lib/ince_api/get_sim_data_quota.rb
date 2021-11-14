d41a781098cf9d0ccb30ad3faf8d3df5c5d8966b
Staszek <s.zawadzki@visuality.pl>
2021-11-14 22:55:30 +0100

    Sim data quota

---
+++ lib/ince_api/get_sim_data_quota.rb
@@ -0,0 +1,36 @@
+module InceApi
+  class GetSimDataQuota
+    def initialize(access_token:, iccid:)
+      @access_token = access_token
+      @iccid = iccid
+    end
+
+    def sim_status
+      response = connection.request(request)
+      if response.body != ''
+        JSON.parse(response.body)
+      else
+        {'status_code' => 404, 'error_message' => 'SIM with ICCID not found'}
+      end
+    end
+
+    private
+
+    def url
+      @url ||= URI("https://api.1nce.com/management-api/v1/sims/#{@iccid}/quota/data")
+    end
+
+    def connection
+      @connection ||= Net::HTTP.new(url.host, url.port).tap do |http|
+        http.use_ssl = true
+      end
+    end
+
+    def request
+      Net::HTTP::Get.new(url).tap do |request|
+        request["Accept"] = 'application/json'
+        request['Authorization'] = "Bearer #{@access_token}"
+      end
+    end
+  end
+end
---
+++ spec/ince-api/get_sim_data_quota_spec.rb
@@ -0,0 +1,12 @@
+require 'spec_helper'
+
+RSpec.describe InceApi::GetSimDataQuota do
+  it 'get one sim data quota' do
+    VCR.use_cassette('get_sim_data_quota') do
+      response = described_class.new(access_token: 'VALID TOKEN', iccid: '8988228066602306770').sim_status
+      expect(response['volume']).to eq 479.949998
+      expect(response['total_volume']).to eq 500
+      expect(response['threshold_percentage']).to eq 20
+    end
+  end
+end
