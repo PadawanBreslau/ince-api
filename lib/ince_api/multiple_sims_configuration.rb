7418e10e3cf834f787cd200a1ab9772672380be3
Staszek <s.zawadzki@visuality.pl>
2021-11-14 22:24:49 +0100

    Update mulitple SIM cards

---
+++ lib/ince_api/multiple_sims_configuration.rb
@@ -0,0 +1,40 @@
+module InceApi
+  class MultipleSimsConfiguration
+    def initialize(access_token:, changes_array: [])
+      @access_token = access_token
+      @changes_array = changes_array
+    end
+
+    def update_many
+      response = connection.request(request)
+      response.code.to_i == 201 ? { status: 'OK' } : { status: 'FAILED', error_code: response.code.to_i}
+    end
+
+    private
+
+    ALLOWED_KEYS = %i(iccid label imei_lock status)
+    def body
+      @changes_array.select{|sim_params| !sim_params[:iccid].nil?}
+        .map{|sim_params| sim_params.slice(*ALLOWED_KEYS)}.to_json
+    end
+
+    def url
+      @url ||= URI("https://api.1nce.com/management-api/v1/sims")
+    end
+
+    def connection
+      @connection ||= Net::HTTP.new(url.host, url.port).tap do |http|
+        http.use_ssl = true
+      end
+    end
+
+    def request
+      Net::HTTP::Post.new(url).tap do |request|
+        request.body = body
+        request["Accept"] = 'application/json'
+        request["Content-Type"] = 'application/json;charset=UTF-8'
+        request['Authorization'] = "Bearer #{@access_token}"
+      end
+    end
+  end
+end
---
+++ spec/ince-api/multiple_sims_configuration_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+RSpec.describe InceApi::MultipleSimsConfiguration do
+  it 'updates one sim card - no changes' do
+    VCR.use_cassette('update_sims_empty_list') do
+      response = described_class.new(access_token: 'VALID TOKEN').update_many
+      expect(response[:status]).to eq 'OK'
+    end
+  end
+
+  it 'updates one sim card - changes' do
+    VCR.use_cassette('update_sims_label_change') do
+      params = [{iccid: '8988228066602306711', label: 'Test Label 1'}, {iccid: '8988228066602307111', label: 'Test Label 2'}]
+      response = described_class.new(access_token: 'VALID TOKEN', changes_array: params).update_many
+      expect(response[:status]).to eq 'OK'
+    end
+  end
+end
